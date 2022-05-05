7069fd615104794a24ff09666eb7ca551a45e24d
Matt Phillips <matt.phillips@gmail.com>
2022-05-04 17:16:34 -0700

    minor clean up and simplification of example
    - Added behavior flag `CONVERT_TO_SET` at top of script to make the demo
      very easy to toggle.
    - Simplified regex to version that isn't too scary for the uninitiated

--- lists-vs-sets/find-unfamiliar-words.py
+++ lists-vs-sets/find-unfamiliar-words.py
@@ -8,13 +8,16 @@ LEXICON = 'sowpods.txt'
 TEST_FILE = 'shakespeare.txt'
 NEW_WORDS_FILE = 'new-words.txt'
 
+# Change below to see a big difference in execution speed
+CONVERT_TO_SET = False
+
 
 def time_execution(func):
     def timed(*args, **kwargs):
         start = time.time()
         result = func(*args, **kwargs)
         end = time.time()
-        print(">>> Time to execute: {}\n".format(end - start))
+        print(f">>> Time to execute: {end - start:.2f} sec\n")
         return result
     return timed
 
@@ -25,10 +28,11 @@ def read_words(filename, label='file'):
     with open(filename) as fin:
         words = []
         for line in fin:
-            # do some basic clean up of punctuation and other non-alphas
-            line = re.sub(r'[,;:?!()\[\]\|/#*$&"’\'_.-]', ' ', line)
+            # do some basic clean up - convert any non word characters to spaces
+            line = re.sub(r'[^\w]', ' ', line)
             for word in line.split():
                 word = word.lower().strip()
+                # ignore empty strings and any values that start with a digit
                 if not word or re.search(r'^\d+', word):
                     continue
                 words.append(word)
@@ -58,8 +62,9 @@ def main():
     literature = read_words(TEST_FILE, label=TEST_FILE)
 
     lexicon = read_words(LEXICON, label="lexicon")
-    # uncomment below to see a big difference in execution speed
-    # lexicon = set(lexicon)
+
+    if CONVERT_TO_SET:
+        lexicon = set(lexicon)
 
     unfamiliar = find_new_words(literature, lexicon, TEST_FILE, False)
     unfamiliar = sorted(list(unfamiliar))
